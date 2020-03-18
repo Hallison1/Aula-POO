5f5c5736891e7f328d4345b920ed56dbfe4af53e
COTEMIG <71900225@aluno.faculdadecotemig.br>
2020-03-17 22:20:04 -0300

    commit 1, foi somente criado um projeto com algumas classe tudo vazio

---
+++ aula-criar computador/src/Program.java
@@ -0,0 +1,11 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		// ADD 3 ATRIBUTOS EM CADA CLASSE E 2 METODOS, AO MENOS 1 ATRIBUTO COMPLEXO
+	}
+
+}
